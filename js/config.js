6810c3fa82b539c0425516bd32e62d4901ca6348
iansun <lauer3912@gmail.com>
2016-11-24 09:52:37 +0800

    update

---
+++ js/config.js
@@ -0,0 +1,15 @@
+(function () {
+    window.RTYConfig = {
+        appName:"NumberHelper",
+        appID:"com.romanysoft.app.macos.NumberHelper",
+        documentTitle:"NumberHelper By Romanysoft",
+        supportPlatforms:["Mac"],
+        googleUA:"UA-87767476-3",
+        gitHome:"//github.com/Romanysoft/NumberHelper",
+        reportIssueUrl:"//github.com/Romanysoft/NumberHelper/issues",
+        changeLogUrl:"//github.com/Romanysoft/NumberHelper/wiki/Changelog",
+        wikiUrl:"//github.com/Romanysoft/NumberHelper/wiki",
+        romanysoftHome:"//www.romanysoft.com",
+        copyright:"Copyright © " + (new Date()).getFullYear() + " Romanysoft LAB."
+    }
+})();
